5d5210d4796eb29e479786f6fd4ec1bc3ce49945
Gino <ginoalodar@gmail.com>
2022-04-23 04:17:16 -0500

    implemented twilio API for sms notifications

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const cors = require('cors');
 
 const authRoutes = require('./routes/auth.js');
+const twilio = require('twilio');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //call environment variables inside the node application
 require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
 
 //cross-origin requests
 app.use(cors());
@@ -18,6 +24,27 @@ app.use(express.urlencoded());
 //first route; root route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.post('/', (req, res) => {
+  const { message, user: sender, type, members} = req.body;
+
+  if(type === 'message.new'){
+    members.filter((member) => member.user.id !== sender.id).forEach(({user}) => {
+      if(!user.online){
+        twilioClient.messages.create({
+          body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+          messagingServiceSid: messagingServiceSid,
+          to: user.phoneNumber
+        })
+          .then(()=> console.log('Message sent!'))
+          .catch((err) => console.log(err));
+      }
+    })
+    return res.status(200).send('Message sent!');
+  }
+
+  return res.status(200).send('not a message req');
 });
 
 app.use('/auth', authRoutes);
